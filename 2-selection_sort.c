61d7d35a3f77ffc189760ff168a145b6358652c4
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-14 13:54:14 -0500

    array of integers in ascending order

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort - array of integers in ascending order
+ * @array: is the list of integers
+ * @size: is the lenght of list
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min, tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (array[i] > array[min])
+		{
+			tmp = array[min];
+			array[min] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
+}
