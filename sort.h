b891da4b687a691cdf3a13fef8cec46a24024843
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-14 19:09:48 -0500

    header files

--- sort.h
+++ sort.h
@@ -25,8 +25,8 @@ void print_array(const int *array, size_t size);
 /* sort funtions */
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-/* void quick_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 void quick_sort_algo(int *array, int low, int high, size_t size);
-int partition(int *array, int low, int high, size_t size); */
+int partition(int *array, int low, int high, size_t size);
 
 #endif
