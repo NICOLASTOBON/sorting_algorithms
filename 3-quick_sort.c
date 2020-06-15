b8d953c8cadf934e6907dd97a4c2fb16ff41ecf0
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-14 20:49:16 -0500

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+/**
+ * swap - swap
+ * @array: list
+ * @low: low
+ * @high: high
+ * @size: size
+ * Return: nothing
+ */
+void swap(int *array, int *low, int *high, size_t size)
+{
+	int tmp;
+
+	tmp = *low;
+	*low = *high;
+	*high = tmp;
+
+	print_array(array, size);
+}
+/**
+ * partition - function partition and return index
+ * @array: list
+ * @low: low
+ * @high: high
+ * @size: size
+ * Return: index
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int j;
+	int pivot = array[high];
+	int i = low;
+
+	for (j = low; j < high ; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+				swap(array, &array[i], &array[j], size);
+			i++;
+		}
+	}
+	if (i != high)
+		swap(array, &array[i], &array[high], size);
+	return (i);
+}
+/**
+ * quick_sort_algo - function quick_sort_algo
+ * @array: list
+ * @low: low
+ * @high: high
+ * @size: size
+ * Return: index
+ */
+void quick_sort_algo(int *array, int low, int high, size_t size)
+{
+	int pt;
+
+	if (low < high)
+	{
+		pt = partition(array, low, high, size);
+
+		quick_sort_algo(array, low, pt - 1, size);
+		quick_sort_algo(array, pt + 1, high, size);
+	}
+}
+/**
+ * quick_sort - quick_sort
+ * @array: list
+ * @size: size
+ * Return: nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quick_sort_algo(array, 0, size - 1, size);
+}
