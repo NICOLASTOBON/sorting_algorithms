84c521d8b501599bafcca9c4acce7cddc997e47b
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-15 23:35:37 -0500

    header files

--- sort.h
+++ sort.h
@@ -26,6 +26,7 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void shell_sort(int *array, size_t size);
 
 /* quick sort funtions */
 void quick_sort(int *array, size_t size);
